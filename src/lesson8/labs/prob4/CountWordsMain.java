02f49fede60476f132bad0423710b5fccda5fa2b
AmosJindao <liusr@outlook.com>
2017-09-19 15:06:10 -0500

    add answers of prob3 and prob4

---
+++ src/lesson8/labs/prob4/CountWordsMain.java
@@ -0,0 +1,26 @@
+package lesson8.labs.prob4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountWordsMain{
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+
+        words.add("abc");
+        words.add("afr");
+        words.add("fgh");
+        words.add("dct");
+        words.add("oku");
+        words.add("jkl");
+
+        System.out.println(countWords(words,'c','d',words.size()));
+    }
+
+    public static int countWords(List<String> words, char c, char d, int len){
+        return (int)words.stream()
+                        .filter(word -> word.contains(c+""))
+                        .filter(word -> !word.contains(d+""))
+                        .count();
+    }
+}
