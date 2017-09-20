c8d5f8a06c070580214cca861277167faaa1e7b6
Administrator <Administrator@172.18.6.96>
2017-09-20 17:49:13 -0500

    1

--- src/lesson9/labs/prob1/business/Main.java
+++ src/lesson9/labs/prob1/business/Main.java
@@ -1,6 +1,8 @@
 package lesson9.labs.prob1.business;
 
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import lesson9.labs.prob1.dataaccess.DataAccess;
 import lesson9.labs.prob1.dataaccess.DataAccessFacade;
@@ -21,8 +23,9 @@ public class Main {
 		Collection<LibraryMember> members = da.readMemberMap().values();
 		List<LibraryMember> mems = new ArrayList<>();
 		mems.addAll(members);
-		//implement
-		return null;
+		List<String> result=mems.stream().filter(w->w.getAddress().getZip().contains("3")).map(w->w.getMemberId()).collect(Collectors.toList());
+		
+		return result;
 		
 	}
 	//Returns a list of all isbns of books having at least two copies
@@ -31,8 +34,8 @@ public class Main {
 			Collection<Book> books = da.readBooksMap().values();
 			List<Book> bs = new ArrayList<>();
 			bs.addAll(books);
-			//implement
-			return null;
+			List<String> result=bs.stream().filter(w->w.getNumCopies()>=2).map(w->w.getIsbn()).collect(Collectors.toList());
+			return result;
 			
 		}
 	
@@ -42,8 +45,8 @@ public class Main {
 		Collection<Book> books = da.readBooksMap().values();
 		List<Book> bs = new ArrayList<>();
 		bs.addAll(books);
-		//implement
-		return null;
+		List<String> result=bs.stream().filter(w->w.getAuthors().size()>1).map(w->w.getIsbn()).collect(Collectors.toList());
+		return result;
 		
 		}
 
